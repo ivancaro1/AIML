ce64622e34878619b5797d9a4d5388a5226146e3
ivan caro <ivanfelipecaro98@gmail.com>
2021-08-10 20:51:08 -0500

    Script de ejemplo de borracho

---
+++ Estadistica_Computacional/ejemplo_borracho/borracho.py
@@ -0,0 +1,12 @@
+class Borracho:
+
+    def __init__(self, name):
+        self.name = name
+
+class BorrachoTradicional(Borracho):
+
+    def __init__(self,name):
+        super().__init__(name)
+
+    def camina():
+        return random.choice([(0, 1),(0,-1),(1,0),(-1,0)])
