fb3f8d79e2a5f93d8247f3c16295562427bd1655
ivan caro <ivanfelipecaro98@gmail.com>
2021-08-10 21:02:16 -0500

    Se importa función random

--- Estadistica_Computacional/ejemplo_borracho/borracho.py
+++ Estadistica_Computacional/ejemplo_borracho/borracho.py
@@ -1,3 +1,5 @@
+import random
+
 class Borracho:
 
     def __init__(self, name):
@@ -8,5 +10,5 @@ class BorrachoTradicional(Borracho):
     def __init__(self,name):
         super().__init__(name)
 
-    def camina():
+    def camina(self):
         return random.choice([(0, 1),(0,-1),(1,0),(-1,0)])
